4368d5fe9ef191a32c2298032118a87d7af3c33d
Quan95vn <tranngocquan95vn@gmail.com>
2017-06-12 23:31:27 +0700

    Lesson11: Configure routing for application

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
+import { Routes,RouterModule } from '@angular/router';
+
+export const routes : Routes = [
+  // localhost:4200/login => goi toi' LoginComponent
+  {path: '',component: LoginComponent},
+]
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    // Cau hinh duong dan ben trong login => localhost:4200/login/...
+    RouterModule.forChild(routes)
   ],
   declarations: [LoginComponent]
 })
