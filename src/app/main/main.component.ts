cd2d3a603614d48d44552aa95377efd5177195f9
Quan95vn <tranngocquan95vn@gmail.com>
2017-06-14 17:32:30 +0700

    Lesson17: Display user details after logged in

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -3,6 +3,8 @@ import { SystemConstants } from '../core/common/system.constants';
 import { UrlConstants } from '../core/common/url.constants';
 
 import { UtilityService } from '../core/services/utility.service';
+import { AuthenService } from '../core/services/authen.service';
+import { LoggedInUser} from '../core/domain/loggedin.user';
 
 @Component({
   selector: 'app-main',
@@ -10,10 +12,15 @@ import { UtilityService } from '../core/services/utility.service';
   styleUrls: ['./main.component.css']
 })
 export class MainComponent implements OnInit {
+  public user: LoggedInUser;
 
-  constructor(private ultilityService: UtilityService) { }
+  constructor(
+    private ultilityService: UtilityService,
+    private authenService: AuthenService)
+  { }
 
   ngOnInit() {
+    this.user = JSON.parse(localStorage.getItem(SystemConstants.CURRENT_USER));
   }
 
   logout() {
