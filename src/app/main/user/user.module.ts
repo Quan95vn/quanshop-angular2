c89d054e782c250c42b0e5d60f56dae76a6db61a
Quan95vn <tranngocquan95vn@gmail.com>
2017-06-15 15:47:17 +0700

    Lesson25: List paginated users

--- src/app/main/user/user.module.ts
+++ src/app/main/user/user.module.ts
@@ -1,19 +1,28 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
+import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { DataService } from '../../core/services/data.service';
+import { NotificationService } from '../../core/services/notification.service';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
 const userRoutes: Routes = [
   // localhost:4200/main/user => localhost:4200/main/user/index
-  { path:'', redirectTo: 'index', pathMatch: 'full'},
+  { path: '', redirectTo: 'index', pathMatch: 'full' },
   // localhost:4200/main/user/index
-  { path: 'index', component: UserComponent}
+  { path: 'index', component: UserComponent }
 ]
 @NgModule({
   imports: [
     CommonModule,
+    PaginationModule,
+    FormsModule,
+    ModalModule.forRoot(),
     RouterModule.forChild(userRoutes)
   ],
-  declarations: [UserComponent]
+  declarations: [UserComponent],
+  providers: [DataService, NotificationService]
 })
 export class UserModule { }
